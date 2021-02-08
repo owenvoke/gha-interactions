481a035f4e9834768a61acd5a36de3df881b07b7
Owen Voke <development@voke.dev>
2021-02-08 17:56:18 +0000

    chore: extract strings into constants

--- src/Concerns/InteractsWithGitHubActionsAnnotations.php
+++ src/Concerns/InteractsWithGitHubActionsAnnotations.php
@@ -8,37 +8,28 @@ use RuntimeException;
 
 trait InteractsWithGitHubActionsAnnotations
 {
-    private function buildAnnotation(string $type, string $message, string $file = null, int $line = null): string
-    {
-        if (! in_array($type, ['error', 'warning', 'debug'])) {
-            throw new RuntimeException('An invalid annotation type was provided');
-        }
+    public const GH_LOG_TYPE_DEBUG = 'debug';
+    public const GH_LOG_TYPE_ERROR = 'error';
+    public const GH_LOG_TYPE_WARNING = 'warning';
 
-        $message = str_replace("\n", '%0A', $message);
-
-        $fileInformation = ($file && $line) ? " file={$file},line={$line}" : null;
-
-        return "::{$type}{$fileInformation}::{$message}";
-    }
-
-    protected function addGitHubActionsWarningMessage(string $message, string $file = null, int $line = null): void
+    protected function addGitHubActionsDebugMessage(string $message): void
     {
         $this->output->writeln(
-            $this->buildAnnotation('warning', $message, $file, $line)
+            $this->buildAnnotation(self::GH_LOG_TYPE_DEBUG, $message)
         );
     }
 
     protected function addGitHubActionsErrorMessage(string $message, string $file = null, int $line = null): void
     {
         $this->output->writeln(
-            $this->buildAnnotation('error', $message, $file, $line)
+            $this->buildAnnotation(self::GH_LOG_TYPE_ERROR, $message, $file, $line)
         );
     }
 
-    protected function addGitHubActionsDebugMessage(string $message): void
+    protected function addGitHubActionsWarningMessage(string $message, string $file = null, int $line = null): void
     {
         $this->output->writeln(
-            $this->buildAnnotation('debug', $message)
+            $this->buildAnnotation(self::GH_LOG_TYPE_WARNING, $message, $file, $line)
         );
     }
 
@@ -57,5 +48,18 @@ trait InteractsWithGitHubActionsAnnotations
         $this->output->writeln(
             "::set-output name={$name}::{$value}"
         );
+    }
+
+    private function buildAnnotation(string $type, string $message, string $file = null, int $line = null): string
+    {
+        if (! in_array($type, [self::GH_LOG_TYPE_DEBUG, self::GH_LOG_TYPE_ERROR, self::GH_LOG_TYPE_WARNING])) {
+            throw new RuntimeException('An invalid GitHub Actions annotation type was provided');
+        }
+
+        $message = str_replace("\n", '%0A', $message);
+
+        $fileInformation = ($file && $line) ? " file={$file},line={$line}" : null;
+
+        return "::{$type}{$fileInformation}::{$message}";
     }
 }
